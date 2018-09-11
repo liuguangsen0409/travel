c97c06093510b9a8301b1b9e6b052603bd9ebd6c
liuguangsen0409 <liuguangsen@renrenche.com>
2018-09-11 21:43:58 +0800

    详情页banner

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="banner">
+    <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1508/89/895a1b7add84f23faca053ce9e3153db.water.jpg_600x330_93d96bdf.jpg">
+    <div class="banner-info">
+      <div class="banner-title">北京欢乐谷(AAAA景区)</div>
+      <div class="banner-number"><span class="iconfont banner-icon">&#xe691;</span>160</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Banner'
+}
+</script>
+
+<style lang="stylus" scoped>
+.banner
+  position: relative
+  overflow: hidden
+  height: 0
+  padding-bottom: 55%
+  .banner-img
+    width: 100%
+  .banner-info
+    display: flex
+    position: absolute
+    left: 0
+    right: 0
+    bottom: 0
+    line-height: .6rem
+    color: #fff
+    background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+    .banner-title
+      flex: 1
+      font-size: .32rem
+      padding: 0 .2rem
+    .banner-number
+      line-height: .32rem
+      height: .32rem
+      margin-top: .14rem
+      padding: 0 .4rem
+      border-radius: .2rem
+      background: rgba(0, 0, 0, .8)
+      font-size: .24rem
+      .banner-icon
+        font-size: .24rem
+</style>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -66,6 +66,6 @@ export default {
 }
 </script>
 
-<style>
+<style lang="stylus" scoped>
 
 </style>
